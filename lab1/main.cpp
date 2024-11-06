272660cb59310ad75b542f83e9ebfc3c254901ea
Redgrxve <nevejin.gosha@yandex.ru>
2024-11-06 19:07:07 +0300

    all tasks done and debugged

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -2,10 +2,13 @@
 
 #include <iomanip>
 #include <iostream>
+#include <windows.h>
+
+#define DEFAULT_PRESCISION 6
 
 double randomNumber(double min, double max)
 {
-    return min + (rand() % (int)(max - min + 1));
+    return (double)rand() / RAND_MAX * (max - min) + min;
 }
 
 void inputArray(double* array, int size)
@@ -14,20 +17,29 @@ void inputArray(double* array, int size)
         std::cin >> array[i];
 }
 
-void fillArrayByFirst(double* array, int size)
+void fillArithmProgression(double* array, int size, double first, double step)
 {
-    std::cin >> array[0];
+    array[0] = first;
     for (int i = 1; i < size; ++i)
-        array[i] = array[i - 1] + 6.3;
+        array[i] = array[i - 1] + step;
 }
 
-void printArray(const double* array, int size,
+void printArray(const double* array,
+                int size,
                 const char* arrayName = "Array",
-                int precision = 5)
+                bool exponential = false,
+                int precision = 6)
 {
+    if (exponential)
+        std::cout << std::scientific;
     std::cout << std::setprecision(precision);
+
     for (int i = 0; i < size; ++i)
         std::cout << arrayName << i + 1 << "\t" << array[i] << '\n';
+
+    if (exponential)
+        std::cout << std::fixed;
+    std::cout << std::setprecision(DEFAULT_PRESCISION);
 }
 
 double averageArray(const double* array, int size)
@@ -44,11 +56,13 @@ double calculateFunction(double x)
 {
     double result;
     if (x < 0) {
-        result = 3 * (x * x) + 3 * x + 8;
-    } else if (x >= 0 && x < 20) {
-        result = -(5 / (x - 8));
-    } else {
-        result = 4 * (x + 8);
+        result = 3.0 * (x * x) + 3.0 * x + 8.0;
+    }
+    else if (x >= 0 && x < 20) {
+        result = -(5.0 / (x - 8.0));
+    }
+    else {
+        result = 4.0 * (x + 8.0);
     }
 
     return result;
@@ -56,44 +70,54 @@ double calculateFunction(double x)
 
 void fillArrayRandom(double* array, int size, double min, double max)
 {
+    srand(time(0));
     for (int i = 0; i < size; ++i)
         array[i] = randomNumber(min, max);
 }
 
 int main()
 {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
     const int XY_SIZE = 8;
     const int X1_MAX_SIZE = 15;
     double x[XY_SIZE];
     double x1[X1_MAX_SIZE];
     double y[XY_SIZE];
 
-    std::cout << "Enter the first element of array: \n";
-    fillArrayByFirst(x, XY_SIZE);
-    std::cout << std::endl;
-    std::cout << "Elements of X: \n";
+    std::cout << "Введите первый элемент массива: ";
+    double first;
+    std::cin >> first;
+    fillArithmProgression(x, XY_SIZE, first, 6.3);
+    std::cout << "Элементы массива X: ";
     printArray(x, XY_SIZE, "x");
-    std::cout << std::endl;
-    std::cout << "Average of X elements: ";
-    std::cout << std::scientific << averageArray(x, XY_SIZE) << '\n';
+
+    printf("Среднее значение элементов массива X: %e\n\n", averageArray(x, XY_SIZE));
 
     for (int i = 0; i < XY_SIZE; ++i)
         y[i] = calculateFunction(x[i]);
-    std::cout << std::fixed;
-    printArray(y, XY_SIZE, "y", 3);
+    std::cout << "Элементы массива Y, рассчитанные по функции F(Xi):\n";
+    printArray(y, XY_SIZE, "y", false, 3);
 
     int x1Size;
+    std::cout << "\nВведите количество элементов в массиве X1: ";
     std::cin >> x1Size;
     while (x1Size < 10 || x1Size > 15) {
-        std::cout << "Size of the array out of bounds! Please, try again\n";
+        std::cout << "Размер массива не может быть больше 15 или меньше 10. Попробуйте еще раз.";
         std::cin >> x1Size;
     }
-    srand(time(0));
-    fillArrayRandom(x1, x1Size, 99, 100);
-    printArray(x1, x1Size, "x1_", 3);
 
-    std::cout << "Average of X elements: ";
-    std::cout << std::scientific << averageArray(x1, x1Size) << '\n';
+    std::cout << "Введите минимальное и максимальное число для заполнения массива случайными значениями.\n";
+    int min, max;
+    std::cout << "Минимальное: ";
+    std::cin >> min;
+    std::cout << "Максимальное: ";
+    std::cin >> max;
+    fillArrayRandom(x1, x1Size, min, max);
+    printArray(x1, x1Size, "x1_");
+
+    printf("Среднее значение элементов массива X1: %e\n", averageArray(x1, x1Size));
 
     return 0;
 }
